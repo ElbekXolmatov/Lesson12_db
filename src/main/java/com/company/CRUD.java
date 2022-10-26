c8f2074feb191d99d9ef6f8ba6459be0d13cb272
KhoEl <exolmatov99@gmail.com>
2022-10-26 17:14:31 +0500

    Fayzullo qoshdi

--- src/main/java/com/company/CRUD.java
+++ src/main/java/com/company/CRUD.java
@@ -9,6 +9,7 @@ import com.company.studentCRUD.UpdateExample;
 public class CRUD {
     public static void main(String[] args) {
 
+//        asd?
         System.out.println("second commit");
         while(true){
             System.out.println("1.C//2.R//3.U//4.D");
